98dd457fc785b9dc05006f97a63129bd7ac1f5e3
Heber <heber@telescopiodigital.com.br>
2021-02-04 14:54:44 -0300

    cria tema

---
+++ wp-content/themes/telescopio/index.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>   
+    <div class="content-area">
+        <main>
+            <section class="projects"></section>
+            <section class="slide"></section>
+            <section class="aboutUs">
+                <aside class="sidebar"></aside>
+                <div class="about"></div>
+            </section>
+            <section class="galeria"></section>
+            <section class="depositions"></section>
+            <section class="blog"></section>
+            <section class="social-media"></section>
+        </main>        
+    </div>
+<?php get_footer(); ?>     
