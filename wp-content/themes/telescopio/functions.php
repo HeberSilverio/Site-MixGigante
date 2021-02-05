eddf2d9f2c0cec1070ace60e3288fb49ff77935f
Héber Silvério <hebersilverio@gmail.com>
2021-02-04 23:23:22 -0300

    add template.css

--- wp-content/themes/telescopio/functions.php
+++ wp-content/themes/telescopio/functions.php
@@ -1,9 +1,9 @@
 <?php
 
 function load_scripts(){ //
-    wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js', array() '5.0.0', true);
+    wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js', array(), '5.0.0', true);
 
-    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css', array(), '5.0.0','all'); // adiciona css
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css', array(), '5.0.0', 'all'); // adiciona css
     wp_enqueue_style('template', get_template_directory_uri() . '/css/template.css', array(), '1.0','all'); // adiciona css
     // wp_enqueue_style('id-do-arquivo-css',get_stylesheet_uri(), array(), '1.0', false); // adiciona o style.css
 }
