409e1631027ed2d6497903b9ebcd9bda39183e19
Heber <heber@telescopiodigital.com.br>
2021-02-05 18:06:55 -0300

    inicio css

--- wp-content/themes/telescopio/footer.php
+++ wp-content/themes/telescopio/footer.php
@@ -1,25 +1,38 @@
-<footer>
-    <aside class="coluna1">
-        <div class="logo">logo</div>
-        <div class="socialIcons">Ícones Sociais</div>
-    </aside>
-    <aside class="coluna2">
-        <div class="itensMenu">Institucional</div>
-    </aside>
-    <aside class="coluna3">
-        <div class="contato">Contato
-            <ul>
-               <li>Telefone: (19) 3016-6374 / (19) 3017-5910</li> 
-               <li>WhatsApp: (19) 99696-0309</li> 
-               <li>E-mail: contato@mixgigante.com.br</li> 
-            </ul>
-            <p>Distrito Industrial de Indaiatuba / SP </br>Rua Platina, 615 - CEP 13347-150</p>
+<footer >
+    <section class="footer">
+        <div class="container">
+            <div class="row">
+            
+                    <div class="coluna1 col-12 col-md-6 col-lg-3">
+                        <div class="logo">Logo
+                        <img src="https://mixgigante.com.br/uploads/logo.png" alt="Facebook">
+                        </div>
+                        <div class="socialIcons">Ícones Sociais</div>
+                    </div>
+                    <nav class="footer-menu col-12 col-md-6 col-lg-3">
+                        <div class="itensMenu">Institucional
+                            <?php wp_nav_menu( array( 'theme_location' => 'my_rodape_menu')); ?>
+                        </div>
+                    </nav>
+                    
+                    <div class="coluna3 col-12 col-md-6 col-lg-3">
+                        <div class="contato">Contato
+                            <ul>
+                                <li>Telefone: (19) 3016-6374 / (19) 3017-5910</li> 
+                                <li>WhatsApp: (19) 99696-0309</li> 
+                                <li>E-mail: contato@mixgigante.com.br</li> 
+                            </ul>
+                            <p>Distrito Industrial de Indaiatuba / SP </br>Rua Platina, 615 - CEP 13347-150</p>
+                        </div>
+                    </div>
+                    <div class="coluna4 col-12 col-md-6 col-lg-3">No Facebook
+                        
+                    </div>
+                    
+                
+            </div>
         </div>
-    </aside>
-    <aside class="coluna4">No Facebook
-        <img src="https://mixgigante.com.br/uploads/logo.png" alt="Facebook">
-    </aside>
-    
+    </section>
 </footer>
 </body>
 </html>
--- wp-content/themes/telescopio/header.php
+++ wp-content/themes/telescopio/header.php
@@ -8,15 +8,83 @@
 </head>
 <body <?php body_class(); ?>>
     <header>
-        <!-- faixa no topo para menu social e search -->
-        <!-- <section class="top-bar">
-            <div class="social-media-icons">Ícones Sociais</div>
-            <div class="search">Pesquisa</div>            
-        </section> -->
+         <!-- faixa no topo para menu social e search -->          
+        
         <section class="menu-area">
-            <section class="logo">Logo
-                <img src="https://mixgigante.com.br/uploads/logo.png" alt="">
-            </section>
-            <nav class="menu">Menu</nav>
+            <div class="container">
+                <div class="row">
+                    <section class="logo col-md-2 col-12 text-left">
+                        <img src="https://mixgigante.com.br/uploads/logo.png" alt="">
+                    </section>
+                    <nav class="menu col-md-10 text-right">
+                        <?php wp_nav_menu( array( 'theme_location' => 'my_main_menu') ); ?>
+                    </nav>
+                </div>
+            </div>  
         </section>
     </header>
+
+    <!-- header
+	<header class="section header">
+		<div class="grid">
+			<-- brand 
+			<a href="#" class="brand" title="Mix Gigante">
+				<img src="https://mixgigante.com.br/uploads/logo.png" alt="Mix Gigante"/>
+			</a> -->
+			<!-- menu mobile 
+			<a href="#" title="Menu" class="menu showMenu">
+				<i class="fa fas fa-bars"></i>
+			</a>
+			<!-- nav 
+			<nav>
+				<!-- mask 
+				<div class="mask showMenu"></div>
+				<!-- close -->
+				<!-- <a href="#" title="Fechar" class="close showMenu">
+					<i class="fa fas fa-window-close"></i>
+				</a>
+				<ul>
+					<li>
+						<a href="#" title="Home">
+							Home
+						</a>
+					</li>
+					<li>
+						<a href="#" title="Quem Somos">
+							Quem Somos
+						</a>
+					</li>
+					<li>
+						<a href="#" title="Portfólio">
+							Portfólio
+						</a>
+					</li>
+					<li>
+						<a href="#" title="Produtos">
+							Produtos
+						</a>
+					</li>
+					<li>
+						<a href="#" title="Clientes">
+							Clientes
+						</a>
+					</li>
+					<li>
+						<a href="#" title="Seja Representante">
+							Seja Representante
+						</a>
+					</li>
+					<li>
+						<a href="#" title="Blog">
+							Blog
+						</a>
+					</li>
+					<li>
+						<a href="#" class="btn" title="Inscreva-se">
+							Solicite um Orçamento
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</header> -->
